bcfa4246081316f046559546c7fef858d9594e9b
77EJ77 <temp221128@gmail.com>
2022-11-30 15:43:06 +0900

    2nd update

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -1,9 +1,19 @@
 package app.src.main.java.dev_java.ch01;
 
 public class Hello {
+
+    int methodA(int i, int j) {
+        int hap;
+        hap = i + j;
+        return hap;
+    }
+
     public static void main(String[] args) {
         System.out.println("git설정 추가");
         System.out.println("Hello Java!!!");
+
+        Hello he = new Hello();
+        he.methodA(1, 3);
     }
 
 }
