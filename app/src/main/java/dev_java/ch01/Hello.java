4b684d0c13812236012791a9024cdfedbc221a79
EJ <temp221128@gmail.com>
2022-11-30 12:44:05 +0900

    test update

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -2,6 +2,7 @@ package app.src.main.java.dev_java.ch01;
 
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("git설정 추가");
         System.out.println("Hello Java!!!");
     }
 
