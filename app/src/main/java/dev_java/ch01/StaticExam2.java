14dc824fba3f0a14a51f9605acef9bc0c1ebb7d9
77EJ77 <temp221128@gmail.com>
2022-11-30 17:50:10 +0900

    1130 last update

---
+++ app/src/main/java/dev_java/ch01/StaticExam2.java
@@ -0,0 +1,23 @@
+package app.src.main.java.dev_java.ch01;
+//static은 변수 앞에 메소드 앞에 올 수 있다.
+
+//static이 있으면 정적변수, 정적 메소드라 한다.
+//인스턴스화 없이 어디서나 호출할 수 있다.
+//non-static에서 호출할 수 있다.
+
+public class StaticExam2 {
+  int i;// 전역변수는 초기화를 생략할 수 있다. 그러면 디폴트 값은 0이다.
+  static int j;
+
+  public static void main(String[] args) {
+    StaticExam1 staex1 = new StaticExam1();
+    StaticExam1 staex2 = new StaticExam1();
+    StaticExam1 staex3 = new StaticExam1();
+    staex1.methodA();
+    staex2.i = 5;
+    staex3.i = 3;
+    System.out.println(staex1.i);// 1
+    System.out.println(staex2.i);// 5
+    System.out.println(staex3.i);// 1 -> 3
+  }
+}
--- app/src/main/java/dev_java/ch01/Variable1.java
+++ app/src/main/java/dev_java/ch01/Variable1.java
@@ -1,17 +1,23 @@
 package app.src.main.java.dev_java.ch01;
 
 public class Variable1 {
+  int x;
+
   // 나는 파라미터가 없는 메소드 입니다. - 꼰대
   // 나는 반환형이 없다.
   void methodA() {
     // 지변은 반드시 초기화를 해야 한다.
     int i;
     i = 1;
-    System.out.println(i);
+    x = i;
+    System.out.println("i = " + i);
   }
 
   public static void main(String[] args) {
     // insert here
-    System.out.println();
+    Variable1 va = new Variable1();
+    va.methodA();
+    System.out.println("x는 :" + va.x);// 인스턴스변수명.변수명 -> 오직 전역변수만 이렇게 쓸 수 있다. non-static
   }
+  // static이 적혀있으면 클래스타입.변수명
 }
